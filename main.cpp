5bb4c52a03c5277799bb1260b69adad31bd57693
konvergentk <44138096+konvergentk@users.noreply.github.com>
2018-11-11 14:45:55 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+/*6) Напишите программу, выписывающую все элементы последовательности,
+имеющие не менее пяти делителей*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int *q = new int[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> *(q + i);
+	};
+	cout << '\n';
+	for (int i = 0; i < n; i++)
+	{
+		int k = 0;
+		int g = *(q + i);
+		for (int j = 1; j <= g; j++)
+		{
+			if ((g % j) == 0)
+			{
+				k++;
+			};
+		};
+		if (k >= 5)
+		{
+			cout << g << " ";
+		};
+	};
+}
