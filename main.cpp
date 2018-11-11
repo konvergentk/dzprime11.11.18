b8bca8506501f54135eae1ef40584ec4f7d3455f
konvergentk <44138096+konvergentk@users.noreply.github.com>
2018-11-11 17:51:49 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,60 @@
+/*18) Напишите логическую функцию, проверяющую, является строка палиндромом.
+Можно считать, что строка состоит только из букв латиницы.*/
+#include <iostream>
+using namespace std;
+
+bool pil (char* A, int b)
+{
+    b--;
+    int k = 0;
+    for (int i = 0; i <= ((b - 1)/2); i++)
+    {
+        if (A[i] != A[b - i])
+        {
+            k++;
+        };
+    };
+    return (k == 0);;
+}
+
+int Length(char* a)
+{
+    int b = 0;
+    while(*a++)
+    {
+        b++;
+    };
+    return b;
+}
+
+int main()
+{
+    int n;
+
+    char* rat = new char [256];
+    cin.getline(rat, 256, '\n');
+    n = Length(rat);
+
+    cout << '\n';
+
+    if (pil(rat, n) == 1)
+    {
+        cout << "Yes";
+    }
+    else
+    {
+        cout << "No";
+    };
+}
+    printf("\n");
+	ss(uh, n);
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+		    if (j == 0) {printf("%1d", uh[i][j]);}
+		    else {printf("%*d", w, uh[i][j]);};
+		};
+		printf("\n");
+	};
+}
